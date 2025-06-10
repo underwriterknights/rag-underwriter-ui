8b7d0fa1037babe67f03894074a94fda26c97952
Ganesh Arumugam <ganeshbabumca2011@gmail.com>
2025-06-09 22:50:28 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    
+    <div >
+      <div className='divTitle'>
+        <img src="/src//assets//Logo.jpg" className='logo'></img>
+        <h3 className='titleName'>Underwriter Knights</h3>
+      </div>     
+      <div className="divheader" >
+      </div>
+      <div  className='divFooter'>
+        <h1></h1>
+      </div>
+     </div>
+    
+  )
+}
+
+export default App
