98b653443504297dae20214113175926abfe27a6
Ganesh Arumugam <ganeshbabumca2011@gmail.com>
2025-06-09 23:19:06 -0400

    installing amplify

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import ChatBot from "react-chatbotify";
+
+
 
 function App() {
   const [count, setCount] = useState(0)
+const flow = {
+    start: {
+      message: "Who are you?",
+      path: "end"
+    },
+    end: {
+      message: (params) => `Hi ${params.userInput}!`,
+      chatDisabled: true
+    }
+  }
+  const settings = {
+    isOpen: true,
+    general: {
+      primaryColor: '#1e88f5',
+      secondaryColor: '#f5421e',
+      fontFamily: 'Arial, sans-serif',
+      embedded: false
+    },
+    audio: {
+      disabled: false,
+    },
+    chatHistory: {
+      storageKey: "concepts_settings"
+    }
+    // other sections
+  };
 
   return (
     
@@ -14,6 +41,7 @@ function App() {
         <h3 className='titleName'>Underwriter Knights</h3>
       </div>     
       <div className="divheader" >
+        <ChatBot settings={settings} flow={flow} />
       </div>
       <div  className='divFooter'>
         <h1></h1>
