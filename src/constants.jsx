711b43c2a6764ea77db9d3e56f6f1f9db6303e21
underwriterknights <underwriterknights@gmail.com>
2025-06-22 12:34:12 -0400

    Update constants.jsx

--- src/constants.jsx
+++ src/constants.jsx
@@ -1,4 +1,4 @@
-export const accessKeyId = "";
-export const secretAccessKey="";
+export const accessKeyId = "AKIA4V2DGELOFJMMFC6F";
+export const secretAccessKey="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";
 export const region= 'us-east-1';
 export const bucketName= "underwriterknights";
